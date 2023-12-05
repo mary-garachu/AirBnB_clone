be2ea0f30316b4adbe6b249f1d56cbc6858d19f0
kipngeno kevin <kipngenokevin254@gmail.com>
2023-12-05 04:45:15 -0800

    re-create an instance with this dictionary representation.

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,7 @@ class BaseModel:
     """BaseModel class defines all common attributes
     /methods for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """initializes Public instance attributes:
         id: string - assign with an uuid when an instance is created:
         created_at: datetime - assign with the current datetime
@@ -19,9 +19,16 @@ class BaseModel:
         updated_at: datetime - assign with the current datetime
         it will be updated every time you change your object
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            del kwargs['__class__']
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
