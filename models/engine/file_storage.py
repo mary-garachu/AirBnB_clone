cd384a40292c8af20abe65cbf65ca90770d394a1
kipngeno kevin <kipngenokevin254@gmail.com>
2023-12-06 00:19:17 -0800

    updated documentation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -26,14 +26,20 @@ class FileStorage:
         self.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
+        """
+        serializes __objects to the JSON file (path: __file_path)
+        when this method is called:
+        - it takes self.__objects dictionary the it loops through
+        for every value of the __objects dictionary, it passes the method
+        to_dict() from the BaseModel class
+        This returns the dictionary representation that can be passed onto JSON
+        otherwise, without this method, it will pass its memory reference alone
+        which will cause an error.
+        After this, it dumps the dictionary into a file using json.dump
+        """
         serialized_objs = {}
         for key, value in self.__objects.items():
-            if hasattr(value, 'to_dict') and callable(
-                    getattr(value, 'to_dict')):
-                serialized_objs[key] = value.to_dict()
-            else:
-                serialized_objs[key] = value
+            serialized_objs[key] = value.to_dict()
         with open(self.__file_path, 'w') as file:
             json.dump(serialized_objs, file)
 
@@ -42,6 +48,27 @@ class FileStorage:
         (only if the JSON file (__file_path) exists
         otherwise do nothing
         If the file doesn’t exist, no exception should be raised)
+
+        __objects look like this:
+
+        {"BaseModel.ee49c413-023a-4b49-bd28-f2936c95460d": {"my_number": 89,
+        "__class__": "BaseModel", "updated_at": "2017-09-28T21:07:25.047381",
+        "created_at": "2017-09-28T21:07:25.047372", "name": "My_First_Model",
+        "id": "ee49c413-023a-4b49-bd28-f2936c95460d"}}
+
+        This will help you understand why we are looping though the content
+        and then passing (**value) as kwargs
+
+        Here is a simplified breakdown:
+        This method is initially called by the storage instance of FileStorage
+        Check __init__.py of models to understand
+        This is what reload does:
+        Takes the file, opens and reads it
+        It performs Json load on the contents of the file
+        Then loops though the loaded content
+        It imports the BaseModel - so to create instances
+        creates instances of the BaseModel using values of the loaded content
+        updates the __objects dictionary with the new instances
         """
         if exists(self.__file_path):
             from models.base_model import BaseModel
