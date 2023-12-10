a51d212e13874da48e30945c8ce81f4943b9665c
kipngeno kevin <kipngenokevin254@gmail.com>
2023-12-10 06:23:18 -0800

    Initial user class

---
+++ models/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module contains class User that inherits from BaseModel
+"""
+from base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+    Public class attributes:
+    email: string - empty string
+    password: string - empty string
+    first_name: string - empty string
+    last_name: string - empty string
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
